daa5be011b868f373b294124fa36c49e47f3785e
Justen Anderson <justen.anderson@gmail.com>
2022-08-02 15:38:32 -0600

    Add console log variables

--- variables.js
+++ variables.js
@@ -5,3 +5,6 @@ var zip = 80918
 var areaCode = 719
 var male = true
 var tall = false
+console.log("Justen lives in " + city + ', ' + state);
+console.log(areaCode + ' is the area code for SoCo.');
+console.log("To say that just is tall would be " + tall);
